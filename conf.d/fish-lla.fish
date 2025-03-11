bfab05b07fd4e6372dbd31e2c7eb08058a31d9e6
Alper Cugun <github@alper.nl>
2025-03-11 12:40:47 +0100

    Black change to push

--- conf.d/fish-lla.fish
+++ conf.d/fish-lla.fish
@@ -1,25 +1,25 @@
 function __fish_lla_install --on-event fish-lla_install
     set -Ux __FISH_LLA_BASE_ALIASES l ll lg lt lb lg ls lh
-    
+
     set -Ux __FISH_LLA_ALIASES
-    
-    set -Ux LLA_STANDARD_OPTIONS "--icons"
+
+    set -Ux LLA_STANDARD_OPTIONS --icons
 
     # Options for the base aliases
     set -Ux LLA_L_OPTIONS
-    set -Ux LLA_LL_OPTIONS "--long"
-    set -Ux LLA_LG_OPTIONS "--git" "--long"
-    set -Ux LLA_LT_OPTIONS "--tree"
-    set -Ux LLA_LB_OPTIONS "--table"
-    set -Ux LLA_LG_OPTIONS "--grid"
-    set -Ux LLA_LS_OPTIONS "--sizemap"
-    set -Ux LLA_LH_OPTIONS "--timeline"
+    set -Ux LLA_LL_OPTIONS --long
+    set -Ux LLA_LG_OPTIONS --git --long
+    set -Ux LLA_LT_OPTIONS --tree
+    set -Ux LLA_LB_OPTIONS --table
+    set -Ux LLA_LG_OPTIONS --grid
+    set -Ux LLA_LS_OPTIONS --sizemap
+    set -Ux LLA_LH_OPTIONS --timeline
 
     for a in $__FISH_LLA_BASE_ALIASES
         set -l opt_name (string join '_' "LLA" (string upper $a) "OPTIONS")
 
         alias --save "$a" "lla \$LLA_STANDARD_OPTIONS \$$opt_name"
- 
+
         set -a __FISH_LLA_ALIASES "$a"
     end
 end
