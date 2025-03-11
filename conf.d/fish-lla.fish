df185d94ddbf585344872f535579aa05dc818a3d
Alper Cugun <github@alper.nl>
2025-03-11 16:38:00 +0100

    Simplify the code massively

--- conf.d/fish-lla.fish
+++ conf.d/fish-lla.fish
@@ -1,27 +1,12 @@
 function __fish_lla_install --on-event fish-lla_install
-    set -Ux __FISH_LLA_BASE_ALIASES l ll lg lt lb lr ls lh
-
-    set -Ux __FISH_LLA_ALIASES
-
-    set -Ux LLA_STANDARD_OPTIONS --icons
-
-    # Options for the base aliases
-    set -Ux LLA_L_OPTIONS
-    set -Ux LLA_LL_OPTIONS --long
-    set -Ux LLA_LG_OPTIONS --git
-    set -Ux LLA_LT_OPTIONS --tree
-    set -Ux LLA_LB_OPTIONS --table
-    set -Ux LLA_LR_OPTIONS --grid
-    set -Ux LLA_LS_OPTIONS --sizemap --include-dirs
-    set -Ux LLA_LH_OPTIONS --timeline
-
-    for a in $__FISH_LLA_BASE_ALIASES
-        set -l opt_name (string join '_' "LLA" (string upper $a) "OPTIONS")
-
-        alias --save "$a" "lla \$LLA_STANDARD_OPTIONS \$$opt_name"
-
-        set -a __FISH_LLA_ALIASES "$a"
-    end
+    alias --save l "lla --icons"
+    alias --save ll "lla --icons --long"
+    alias --save lg "lla --icons --git"
+    alias --save lt "lla --icons --tree"
+    alias --save lb "lla --icons --table"
+    alias --save lr "lla --icons --grid"
+    alias --save ls "lla --icons --sizemap --include-dirs"
+    alias --save lh "lla --icons --timeline"
 end
 
 function __fish_lla_update --on-event fish-lla_update
@@ -30,12 +15,12 @@ function __fish_lla_update --on-event fish-lla_update
 end
 
 function __fish_lla_uninstall --on-event fish-lla_uninstall
-    set --erase LLA_STANDARD_OPTIONS
-
-    for a in $__FISH_LLA_ALIASES
-        functions --erase $a
-    end
-
-    set --erase __FISH_LLA_BASE_ALIASES
-    set --erase __FISH_LLA_ALIASES
+    functions --erase l
+    functions --erase ll
+    functions --erase lg
+    functions --erase lt
+    functions --erase lb
+    functions --erase lr
+    functions --erase ls
+    functions --erase lh
 end
